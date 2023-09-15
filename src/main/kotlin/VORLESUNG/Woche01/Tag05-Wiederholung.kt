ab6483b961531af45a58af1d2cc9611508a1caa2
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-09-15 08:46:19 +0100

    Tag 5 Livebeispiel: Thema Github

---
+++ src/main/kotlin/VORLESUNG/Woche01/Tag05-Wiederholung.kt
@@ -0,0 +1,48 @@
+package VORLESUNG.Woche01
+
+fun main(){
+
+    // Tag 1: IntelliJ, Github
+        // Wie pulle ich ein neues Projekt?
+            // im Browser auf Github auf das Projekt, das gepullt werden soll -> rechts auf den Code-Button klicken -> Link kopieren -> in IntelliJ: File > New > Project from Version Control
+
+        // Wie pulle ich in ein vorhandenes Projekt?
+            // oben auf Git > Pull > Pull
+
+        // Wie mache ich einen Commit?
+            // Links auf den Github Reiter (unter dem Ordner)
+            // Dateien auswählen
+            // vernünftige Commit Message schreiben, die beschreibt, was ihr gemacht habt
+            // commit und push
+
+        // Was ist die beste Vorgehensweise bei Commits?
+            // Warnungen ignorieren
+            // jede Aufgabe einzeln committen
+            // generell: mehrere kleine Commits, statt einen riesigen -> dann findet man einzelne Zwischenschritte besser
+
+
+
+    // Tag 2: Variablen und Datentypen
+
+        // String
+
+        // Int
+    var x = 1
+
+        // Double
+
+        // Boolean
+
+    // Tag 3: Operationen und Typkonvertierung
+
+        // Rechnen
+
+        // Typ konvertieren
+
+        // String-Konkatenation (Verkettung)
+
+
+    // Tag 4: Debugging
+
+
+}
