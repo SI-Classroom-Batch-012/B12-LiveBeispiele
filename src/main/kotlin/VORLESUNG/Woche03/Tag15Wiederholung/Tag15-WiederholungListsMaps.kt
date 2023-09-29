f33a4bba07b6021c6799973e99e1642d381134da
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-09-29 09:44:40 +0100

    Tag 15: Wiederholung, Reference Type vs Value Type in der VL geschafft

---
+++ src/main/kotlin/VORLESUNG/Woche03/Tag15Wiederholung/Tag15-WiederholungListsMaps.kt
@@ -0,0 +1,32 @@
+package VORLESUNG.Woche03.Tag15Wiederholung
+
+fun main(){
+
+
+    // 1 Element zu Liste hinzufuegen
+
+    // neuen Preis setzen
+
+    // Preis rausholen
+
+    // 1 Element zu Map hinzufuegen
+
+    // neuen Preis setzen
+
+    // Preis rausholen
+
+
+
+
+    // Element mit tiefstem Preis identifizieren: Wie komme ich bei einer Map da ran?
+
+
+    // Wie bei einer Liste?
+
+
+
+
+
+}
+
+
