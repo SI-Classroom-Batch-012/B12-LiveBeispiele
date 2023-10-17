921474f2625e969f49cf795065f8ed68b6e2088e
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-10-17 09:31:55 +0100

    Vorher: Sekundaerer Konstruktor

---
+++ src/main/kotlin/VORLESUNG/Woche06/Tag27/Konstruktorvariable.kt
@@ -0,0 +1,15 @@
+package VORLESUNG.Woche06.Tag27
+
+class Person {
+
+
+    constructor(firstName: String, lastName: String){
+        // init logik, wird immer aufgerufen, sobald eine Instanz Person erstellt wird
+    }
+}
+
+fun main() {
+    var person: Person = Person("Max", "Mustermann")
+
+}
+
